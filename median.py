a658e81fe373860c4ae0e98d6e1fd34c5d598e42
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2022-09-27 19:32:12 +0100

    remove print statements

--- median.py
+++ median.py
@@ -11,12 +11,10 @@ def median(list):
         print((num1+num2)/2)
 while True:
     try:
-        print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
 median(numbers)
 
