2c634f290cc9f09749c3d502d06fa130fd006a1c
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2022-09-27 19:25:59 +0100

    fixed for autograding

--- median.py
+++ median.py
@@ -13,9 +13,10 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        median(numbers)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
+print(numbers)
+median(numbers)
 
